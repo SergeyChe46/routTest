1aea05e1803abf4048a768ee29d2549a988870f7
Sergey Chevychelov <sergeyche46@mail.ru>
2024-03-05 18:29:53 +0300

    add first components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { SecondComponent } from './components/second/second.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SecondComponent
   ],
   imports: [
     BrowserModule,
